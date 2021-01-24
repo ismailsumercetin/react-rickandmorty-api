705e38f4c5f969e654a023c85b7154df8c6e5ef3
sumercetin <sumer@jotform.com>
2021-01-24 23:13:31 +0300

    RickAndMortyApp :: api.js implement with endpoints

---
+++ src/api.js
@@ -0,0 +1,27 @@
+const BASE_URL = new URL("https://rickandmortyapi.com/api/");
+const URLS = {
+    characters: new URL("character",BASE_URL),
+    locations: new URL("location",BASE_URL),
+    episodes: new URL("episode",BASE_URL),
+}
+
+export const getCharacters = async () => {
+    const data = await fetch(URLS.characters)
+        .then(res => res.json());
+
+    return data;
+}
+
+export const getLocations = async () => {
+    const data = await fetch(URLS.locations)
+        .then(res => res.json());
+
+    return data;
+}
+
+export const getEpisodes = async () => {
+    const data = await fetch(URLS.episodes)
+        .then(res => res.json());
+
+    return data;
+}
